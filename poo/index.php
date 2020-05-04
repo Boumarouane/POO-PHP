d6d25a9efd304b1f693d924d5a5e3a010340732c
Boumarouane <i.boumarouane@gmail>
2020-05-04 10:45:01 +0200

    l'encapsulation

--- poo/index.php
+++ poo/index.php
@@ -5,13 +5,30 @@ class Employe {
     // Des variables dans un objet sont appelées des propriétées
     public $nom;
     public $prenom;
-    public $age;
+    // le "private" permet de rendre une propriété modifiable juste à l'intérieur d'une classe, comme ca personne ne peut la changer de l'extérieur.
+    private $age;
     
     // Le constructeur va nous permètre de donner le comportement que doit avoir notre objet en paramètre
     public function __construct($prenom, $nom, $age){
         $this->nom = $nom;
         $this->prenom = $prenom;
-        $this->age = $age;
+        // Ici on vérifie si l'age est bien un nombre grace à la méthode setAge() que j'ai défini, au cas ou la personne aurai envie de modifier la propriétée via le parametre de mon objet Employe().
+        $this->setAge($age);
+    }
+
+    // Permet à qq d'extèrieur de pouvoir changer la propriété ->age sous une condition
+    public function setAge($age){
+        if(is_int($age) && $age > 1 && $age < 120){
+        $this->age = $age ;
+        }
+        else{
+            throw new Exception("L'age de l'employe doit etre un entier entre 1 et 120!");
+        }
+    }
+
+    // Permet de retouner la propriété ->age
+    public function getAge(){
+        return $this->age;
     }
 
     // Une fonction dans un objet est appelé méthode.
@@ -24,6 +41,9 @@ class Employe {
 // Un objet donne une réalitée à la class
 $employe1 = new Employe("Lior", "Chamla", 32);
 $employe2 = new Employe("Issam", "Boumi", 25);
+
+// la personne externe qui va essayer de changer la propriété ->age
+$employe1->setAge(50);
 
 // On accède à l'objet pour lancer ca méthode "présentation()".
 $employe1->presentation();
